e948c0c18753aa5537c3ebdd3401404a2ff7cdab
Roman Berger <romanberger001@gmail.com>
2023-11-06 20:09:46 +0000

    basic keymaps

---
+++ plugin/default.lua
@@ -0,0 +1,11 @@
+vim.opt.relativenumber = false
+
+vim.keymap.set("n", "<leader>l", "<C-W>l")
+vim.keymap.set("n", "<leader>h", "<C-W>h")
+vim.keymap.set("n", "<leader>j", "<C-W>j")
+vim.keymap.set("n", "<leader>k", "<C-W>k")
+
+vim.keymap.set("n", "<leader>s", "<C-W>s <C-W>j")
+vim.keymap.set("n", "<leader>v", "<C-W>v <C-W>l")
+
+vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")
