8e5be126ff50e87f1bc5a8c3239e26dc139cdc8c
Roman Berger <romanberger001@gmail.com>
2024-06-10 22:24:00 +0100

    colors

--- lua/plugins/color_scheme.lua
+++ lua/plugins/color_scheme.lua
@@ -1,16 +1,64 @@
 return {
   {
-    "catppuccin/nvim", 
-    name = "catppuccin", 
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     config = function()
 	require("catppuccin").setup({
+	    color_overrides = {
+	      mocha = {
+		-- custom everforest dark hard port
+		rosewater = "#fed1cb",
+		flamingo  = "#ff9185",
+		pink      = "#d699b6",
+		mauve     = "#cb7ec8",
+		red       = "#e06062",
+		maroon    = "#e67e80",
+		peach     = "#e69875",
+		yellow    = "#d3ad63",
+		green     = "#b0cc76",
+		teal      = "#6db57f",
+		sky       = "#7fbbb3",
+		sapphire  = "#60aaa0",
+		blue      = "#59a6c3",
+		lavender  = "#e0d3d4",
+		text      = "#e8e1bf",
+		subtext1  = "#e0d7c3",
+		subtext0  = "#d3c6aa",
+		overlay2  = "#9da9a0",
+		overlay1  = "#859289",
+		overlay0  = "#6d6649",
+		surface2  = "#585c4a",
+		surface1  = "#414b50",
+		surface0  = "#374145",
+		base      = "#1f2428",
+		mantle    = "#161b1d",
+		crust     = "#14181a",
+	      },
+	    },
 	    integrations = {
 		cmp = true,
 		gitsigns = true,
 		harpoon = true,
 		mason = true,
-		native_lsp = { enabled = true },
+		native_lsp = {
+		    enabled = true,
+		    virtual_text = {
+			errors = { "italic" },
+			hints = { "italic" },
+			warnings = { "italic" },
+			information = { "italic" },
+		    },
+		    underlines = {
+			errors = { "underline" },
+			hints = { "underline" },
+			warnings = { "underline" },
+			information = { "underline" },
+		    },
+		    inlay_hints = {
+			background = true,
+		    },
+		},
 		notify = true,
 		nvimtree = true,
 		telescope = true,
@@ -18,7 +66,7 @@ return {
 		treesitter_context = true,
 	    },
 	})
-	vim.cmd.colorscheme("catppuccin-macchiato")
+	vim.cmd.colorscheme("catppuccin-mocha")
     end,
   },
 }
--- lua/user/remaps.lua
+++ lua/user/remaps.lua
@@ -10,5 +10,9 @@ vim.keymap.set("n", "<leader>k", "<C-W>k")
 
 vim.keymap.set("n", "<leader>s", "<C-W>s <C-W>j")
 vim.keymap.set("n", "<leader>v", "<C-W>v <C-W>l")
+vim.keymap.set({"n", "v"}, "<UP>", "<NOP>")
+vim.keymap.set({"n", "v"}, "<DOWN>", "<NOP>")
+vim.keymap.set({"n", "v"}, "<RIGHT>", "<NOP>")
+vim.keymap.set({"n", "v"}, "<LEFT>", "<NOP>")
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")
